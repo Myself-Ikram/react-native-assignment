5b004189c36850700b74632878fa4404e4603b6d
Myself-Ikram <shadabqhadripubg6@gmail.com>
2024-08-03 12:32:06 +0530

    installed required dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text className="bg-red-200">Apps</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
